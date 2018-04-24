48cd205a249601eae4b436175aa5aa5d3a6810bb
= <=>
2018-04-23 23:15:44 -0300

    added interruptions

--- src/pages/Create/index.vue
+++ src/pages/Create/index.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="container">
     <div class="item">
-      <video ref="video" class="screen b">
-      </video>
+      <div>
+        {{ message }}
+      </div>
+      <div>
+        <video
+          ref="video"
+          class="screen b"
+          :src="video.src"
+          @playing="isPlaying = true"
+          @pause="isPlaying = false"
+          @loadeddata="loadedData($event.target)"
+          @timeupdate="showInterruptions($event.target)"
+        />
+      </div>
+      <div>
+        <input type="range" v-model="time.current" :max="duration" @change="changeTime(time.current)">
+      </div>
+      <div>
+        <button @click="play()" v-if="!isPlaying" :disabled="!loaded">Play</button>
+        <button @click="pause()" v-if="isPlaying" :disabled="!loaded">Pause</button>
+      </div>
     </div>
     <div class="item">
       <div class="screen b">
@@ -10,11 +29,11 @@
           <label for="video_src">Video Source</label>
           <input id="video_src" type="text" v-model="video.src">
         </div>
-        <div class="option">
+        <div class="option b">
           <form v-on:submit.prevent="addInterruption(interruption)">
             <div>
               <label for="video_interruption_message">Interruption Message</label>
-              <input id="video_interruption" type="text" v-model="interruption.message">
+              <input id="video_interruption_message" type="text" v-model="interruption.message">
             </div>
             <div>
               <label for="video_interruption_pause">Interruption Pause</label>
@@ -22,13 +41,16 @@
             </div>
             <div>
               <label for="video_interruption_time">Interruption Time</label>
-              <input type="number" v-model="interruption.time">
-              <button type="button">Get Current Time</button>
+              <input type="number" v-model="interruption.time" step="0.000001">
+              <button type="button" @click="getCurrentTime()">Get Current Time</button>
             </div>
             <div>
               <button type="submit" name="button">Add Interruption</button>
             </div>
           </form>
+        </div>
+        <div>
+          <button type="button" name="button" @click="save(video)">Save Configurations</button>
         </div>
       </div>
     </div>
@@ -39,8 +61,66 @@ export default {
   name: "Create",
   data () {
     return {
-      video: {},
-      interruption: {}
+      video: {
+        src: null,
+        interruptions: []
+      },
+      interruption: {
+        message: '',
+        pause: false,
+        time: 0
+      },
+      isPlaying: false,
+      loaded: false,
+      time: {
+        old: 0,
+        current: 0
+      },
+      duration: 0,
+      message: ''
+    }
+  },
+  methods: {
+    play () {
+      this.$refs['video'].play()
+    },
+    pause () {
+      this.$refs['video'].pause()
+    },
+    addInterruption (interruption) {
+      this.video.interruptions.push(interruption)
+    },
+    save (video) {
+      console.log(video)
+      this.$store.commit('setVideo', video)
+    },
+    getCurrentTime () {
+      const { currentTime } = this.$refs['video']
+      this.interruption.time = currentTime
+    },
+    loadedData (video) {
+      this.duration = video.duration
+      this.loaded = true
+    },
+    showInterruptions (video) {
+      this.time.old = this.time.current
+      this.time.current = video.currentTime
+      const index = this.video.interruptions.findIndex(x => (x.time >= this.time.old && x.time <= this.time.current))
+      if (index >= 0) {
+        this.message = this.video.interruptions[index].message
+        if (this.video.interruptions[index].pause) video.pause()
+      }
+    },
+    changeTime (time) {
+      this.message = ''
+      this.$refs['video'].currentTime = time
+    }
+  },
+  watch: {
+    'video.src': function () {
+      this.loaded = false
+      this.duration = 0
+      this.$refs['video'].load()
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -7,8 +7,7 @@ export default new VueX.Store({
   state: {
     video: {
       src: null,
-      interruptions: [],
-      messages: []
+      interruptions: []
     }
   },
   mutations: {
